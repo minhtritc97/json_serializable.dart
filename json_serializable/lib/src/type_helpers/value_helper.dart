4f8d493d272481b7537e9edc73d521008469d69b
Minh Tri <minhtritc97@gmail.com>
2023-11-16 14:59:04 +0700

    feat: return null if can not parse json value to dart type

--- json_serializable/lib/src/type_helpers/value_helper.dart
+++ json_serializable/lib/src/type_helpers/value_helper.dart
@@ -42,13 +42,26 @@ class ValueHelper extends TypeHelper {
     } else if (targetType.isDartCoreObject || targetType is DynamicType) {
       // just return it as-is. We'll hope it's safe.
       return expression;
-    } else if (targetType.isDartCoreDouble) {
-      final targetTypeNullable = defaultProvided || targetType.isNullableType;
-      final question = targetTypeNullable ? '?' : '';
-      return '($expression as num$question)$question.toDouble()';
-    } else if (simpleJsonTypeChecker.isAssignableFromType(targetType)) {
+    }
+    //  else if (targetType.isDartCoreDouble) {
+    //   final targetTypeNullable = defaultProvided || targetType.isNullableType;
+    //   final question = targetTypeNullable ? '?' : '';
+    //   return '($expression as num$question)$question.toDouble()';
+    // }
+    else if (simpleJsonTypeChecker.isAssignableFromType(targetType)) {
       final typeCode = typeToCode(targetType, forceNullable: defaultProvided);
-      return '$expression as $typeCode';
+      switch (typeCode) {
+        case 'int?':
+          return 'double.tryParse($expression.toString())?.toInt()';
+        case 'double?':
+          return 'double.tryParse($expression.toString())';
+        case 'String?':
+          return '$expression?.toString()';
+        case 'bool?':
+          return 'bool.tryParse($expression.toString())';
+        default:
+          return '$expression as $typeCode';
+      }
     }
 
     return null;
